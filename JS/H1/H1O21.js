31c487eaf282415c9e76c15d7ec5ddb0905f5464
ShivaHVC <145122006+ShivaHVC@users.noreply.github.com>
2023-11-24 12:56:10 +0100

    Update H1O21.js

--- JS/H1/H1O21.js
+++ JS/H1/H1O21.js
@@ -3,7 +3,7 @@ var snelheid = 5;
 var diameter = 200;
 
 function setup() {
-  canvas = createCanvas(1000, 300);
+  canvas = createCanvas(1000,300);
   canvas.parent('processing');
   textFont("Verdana");
   textSize(30);
@@ -14,29 +14,34 @@ function draw() {
   background('orange');
   fill('white');
   noStroke();
-  rect(0, 0, width, 40);
-  fill('black');
-  text("De bol beweegt heen en weer (snelheid = " + snelheid + "). | Klik met je muis!", 10, 30);
+  rect(0,0,width,40);
+  fill('black');  
+  text("De bol beweegt heen en weer (snelheid = " + snelheid + "). | Klik met je muis!",10,30);
   stroke('white');
   strokeWeight(10);
 
-  if ((x > 880 || x < 120) || mouseIsPressed || snelheid > 0) {
-    snelheid = -1 * snelheid;
-  }
-
-  if (mouseIsPressed) {
-    fill('red');
-    diameter = 100;
-  } else if (snelheid > 0) {
+  if ( snelheid == 5  ) {
     fill('green');
-    diameter = 200;
-  } else {
-    fill('dodgerblue');
-    diameter = 200;
+}
+ 
+  else {
+    fill('dodgerblue') ;
   }
+ 
+ 
+if (mouseIsPressed == true  ) {
+  fill('red'), diameter = 100 ;
+}
+else {
+  diameter = 200 ;
+}
+
+  if (x>880 || x<120 ) {
+    snelheid = -1*snelheid;
+  }
+ 
 
   x += snelheid;
-  ellipse(x, 170, diameter);
+  ellipse(x,170,diameter);
 }
-je muis!",10,30);
-}
+
