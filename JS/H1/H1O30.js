aff2c6d9f7100a62238fb7ee3923adc3ab6db328
ShivaHVC <145122006+ShivaHVC@users.noreply.github.com>
2023-12-08 12:31:22 +0100

    i

--- JS/H1/H1O30.js
+++ JS/H1/H1O30.js
@@ -1,14 +1,25 @@
 var rij = 0;
+var oranjeKolom = 6; // Kolomnummer van de oranje gemarkeerde cel
+var oranjeRij = 3; // Rijnummer van de oranje gemarkeerde cel
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(451, 451);
   canvas.parent('processing');
   background('coral');
   fill('white');
 }
 
 function draw() {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      rect(kolom,rij,50,50);
+  for (var rij = 0; rij < 450; rij += 50) {
+    for (var kolom = 0; kolom < 450; kolom += 50) {
+      if (rij === oranjeRij * 50 && kolom === oranjeKolom * 50) {
+        fill('orange');
+      } else {
+        fill('white');
+      }
+      rect(kolom, rij, 50, 50);
     }
+  }
 }
+
+
--- JS/H1/H1O31.js
+++ JS/H1/H1O31.js
@@ -1,16 +1,28 @@
-kleur='white';
+var kleur = 'white'; // Initiële kleur
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(501, 501);
   canvas.parent('processing');
   background('coral');
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
+  for (var rij = 0; rij < 500; rij += 50) {
+    for (var kolom = 0; kolom < 500; kolom += 50) {
       fill(kleur);
-      rect(kolom,rij,50,50);
+      rect(kolom, rij, 50, 50);
+      // Wissel de kleur na elke cel
+      if (kleur === 'white') {
+        kleur = 'black';
+      } else {
+        kleur = 'white';
+      }
+    }
+    // Extra kleurwissel aan het einde van elke rij voor het dambordpatroon
+    if (kleur === 'white') {
+      kleur = 'black';
+    } else {
+      kleur = 'white';
     }
   }
 }
